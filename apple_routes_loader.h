8b8f38c1d89b909066b7c4ec7039d71042c8207c
Calvin Hou <houcal@gmail.com>
2021-11-12 13:23:09 -0500

    Get App into a working state

--- apple_routes_loader.h
+++ apple_routes_loader.h
@@ -4,15 +4,26 @@
 #include "crow.h"
 
 class AppleRoutesLoader {
-    public:
-        template <typename ... Middlewares>
-        void add_routes(crow::App<Middlewares...> &app) {
 
-            CROW_ROUTE(app,"/hello2/<int>")
+    private:
+        int tally_count = 0;
+    public:
+        void add_routes(crow::SimpleApp &app) {
+
+            CROW_ROUTE(app, "/")([](){
+                return "Hello world";
+            });
+
+            CROW_ROUTE(app,"/hello/<int>")
             ([](int count){
                 return crow::response(std::to_string(count));
             });
 
+            CROW_ROUTE(app,"/tally")
+            ([&tally_count = this->tally_count](){
+                tally_count++;
+                return crow::response(std::to_string(tally_count));
+            });
 
         }
     private:
--- gateway.cpp
+++ gateway.cpp
@@ -5,31 +5,6 @@
 #include "crow.h"
 #include "apple_routes_loader.h"
 
-template <typename ... Middlewares>
-void add_routes(crow::App<Middlewares...> &app) {
-
-    // int a =1;
-    // app.port(1);
-    // CROW_ROUTE(app,"/");
-    app.route<crow::black_magic::get_parameter_tag("/")>("/");
-    // ([](){
-    //     return "Hello world";
-    // });
-
-
-}
-
-void add_routes2(crow::SimpleApp &app) {
-
-    // CROW_ROUTE(app,"/hello3/<int>")
-    app.route<crow::black_magic::get_parameter_tag("/hello3/<int>")>("/hello3/<int>")
-    ([](int count){
-        return crow::response(std::to_string(count));
-    });
-
-
-}
-
 int main() {
     std::cout << "Hello World!";
 
@@ -37,27 +12,8 @@ int main() {
 
     AppleRoutesLoader apple_routes_loader;
 
-    CROW_ROUTE(app, "/")([](){
-        return "Hello world";
-    });
-
-    CROW_ROUTE(app,"/hello/<int>")
-    ([](int count){
-        return crow::response(std::to_string(count));
-    });
-
-    int tally_count = 0;
-
-    CROW_ROUTE(app,"/tally")
-    ([&tally_count](){
-        tally_count++;
-        return crow::response(std::to_string(tally_count));
-    });
-
-    // apple_routes_loader.add_routes(app);
-    add_routes(app);
-    add_routes2(app);
+    apple_routes_loader.add_routes(app);
 
     app.port(18080).multithreaded().run();
-    // return 0;
+    return 0;
 }
